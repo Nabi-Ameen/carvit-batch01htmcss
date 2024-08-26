21366267ede2fa61e3956d1178907532a5e529e3
Nabi-Ameen <nabiameen35126@gmail.com>
2024-08-26 19:23:40 +0500

    javascritp variables

---
+++ index.js
@@ -0,0 +1,13 @@
+let firstName = "ali";
+let lastName = "khan";
+let Rollno = 1234;
+let address = "abc123";
+
+let std1 = {
+  firstName: "ali",
+  lastName: "khan",
+  Rollno: 1234,
+  address: "abc123",
+};
+
+console.log(typeof num3);
