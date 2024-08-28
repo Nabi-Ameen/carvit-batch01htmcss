899ae3ea71ce547dd28aec212d2015dd47a78ea9
Nabi-Ameen <nabiameen35126@gmail.com>
2024-08-28 19:25:45 +0500

    javascript array

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
-let person = {
-  firstName: "majid",
-  lastName: "Khan",
-  contactNo: "092308097562",
-  age: 30,
-  address: {
-    address1: "office address",
-    address2: "home address",
-  },
-};
+// let person = {
+//   firstName: "majid",
+//   lastName: "Khan",
+//   contactNo: "092308097562",
+//   age: 30,
+//   address: {
+//     address1: "office address",
+//     address2: "home address",
+//   },
+// };
 
-delete person.firstName;
-
-person.nationality = "Pakistani";
-console.log(person);
-
-// console.log(person.address);
-
+// delete person.firstName;
+// person.nationality = "Pakistani";
 // console.log(person);
 
+// console.log(person.address);
+// console.log(person);
 // window.alert();
+// let numbers = [1, 2, 3, 4, 5, 6, 7];
+
+// let numbers = [10, 20, 30, 40, 50, 60, 70];
+let names = ["ali", "amjad", "uzair"];
+for (let index = 0; index <= names.length - 1; index++) {
+  debugger;
+  console.log(names[index]);
+}
