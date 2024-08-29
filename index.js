2116dfb3f060a5130e97695ddb87c6abfb1b3c9d
Nabi-Ameen <nabiameen35126@gmail.com>
2024-08-29 18:27:56 +0500

    js arrray

--- index.js
+++ index.js
@@ -19,6 +19,7 @@
 // let numbers = [1, 2, 3, 4, 5, 6, 7];
 
 // let numbers = [10, 20, 30, 40, 50, 60, 70];
+
 let names = ["ali", "amjad", "uzair"];
 for (let index = 0; index <= names.length - 1; index++) {
   debugger;
