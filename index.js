f07198daaffad7cd75d88b0a9efc2a0e07a17521
Nabi-Ameen <nabiameen35126@gmail.com>
2024-08-29 19:27:37 +0500

    function and conditional statments

--- index.js
+++ index.js
@@ -20,8 +20,46 @@
 
 // let numbers = [10, 20, 30, 40, 50, 60, 70];
 
-let names = ["ali", "amjad", "uzair"];
-for (let index = 0; index <= names.length - 1; index++) {
-  debugger;
-  console.log(names[index]);
+// let names = ["ali", "amjad", "uzair"];
+// for (let index = 0; index <= names.length - 1; index++) {
+//   debugger;
+//   console.log(names[index]);
+// }
+
+function sum(num1, num2, num3) {
+  let result = num1 + num2 + num3;
+  return result;
 }
+
+// console.log("outside function", sum(10, 20, 30));
+// console.log("outside function", sum(100, 200, 300));
+
+// arrow function syntax
+const multiply = (num1, num2) => {
+  let result = num1 * num2;
+  return result;
+};
+multiply(10, 20);
+
+// const marks = 40;
+// if (marks > 40 && marks < 50) {
+//   console.log("grade D");
+// } else if (marks > 50 && marks < 60) {
+//   console.log("grade C");
+// } else if (marks > 60 && marks < 70) {
+//   console.log("grade B");
+// } else {
+//   console.log("u r failed");
+// }
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const checkElement = (numberArray) => {
+  for (let i = 0; i <= numberArray.length - 1; i++) {
+    if (i <= 5) {
+      console.log(numberArray[i]);
+    }
+  }
+};
+
+checkElement(numbers);
